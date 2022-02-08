d16c6ebe427fe95815a92f04dd700cbeb86be9aa
Kilerd <blove694@gmail.com>
2022-02-08 13:14:26 +0800

    draw: add zh punc func

---
+++ readme.md
@@ -0,0 +1,4 @@
+# chinese word
+
+
+inspected by [woct0rdho/typeset](https://github.com/woct0rdho/typeset)
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,41 @@
+use phf::{Set, phf_set};
+
+
+static ZH_LEFT_PUNC_SET: Set<char> = phf_set! {'（', '【','《','￥'};
+static ZH_RIGHT_PUNC_SET: Set<char> = phf_set! {'，','。','？','！','：','；','）','】','》'};
+static ZH_MIDDLE_PUNC_SET: Set<char> = phf_set! {'·','～','—','…'};
+
+pub fn normalize(content: impl Into<String>) -> String {
+    content.into()
+}
+
+
+fn is_zh_letter(letter: &char) -> bool {
+    letter >= &'\u{4e00}' && letter <= &'\u{9fa5}'
+}
+
+fn is_zh_left_punc(letter: &char) -> bool {
+    ZH_LEFT_PUNC_SET.contains(letter)
+}
+
+fn is_zh_right_punc(letter: &char) -> bool {
+    ZH_RIGHT_PUNC_SET.contains(letter)
+}
+fn is_zh_middle_punc(letter: &char) -> bool {
+    ZH_MIDDLE_PUNC_SET.contains(letter)
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::normalize;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn should_keep_the_same_given_only_english_word() {
+        assert_eq!("hello world", normalize("hello world"));
+    }
+
+    #[test]
+    fn should_remove_duplicated_space_given_multiple_space_word() {
+        assert_eq!("hello world", normalize("hello  world"));
     }
 }
